c6801552dec03c88a5a5af6006bca2d1b2e332f1
seymur <3616126sm>
2021-05-10 20:52:23 +0400

    First commit

---
+++ Assets/EndlessTurns/Scripts/GradientBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientBack : MonoBehaviour
+{
+ public Color color1 = Color.red;
+    public Color color2 = Color.blue;
+    public float duration = 3.0F;
+    
+    Camera camera;
+    
+    void Start() {
+    	camera = GetComponent<Camera>();
+    	camera.clearFlags = CameraClearFlags.SolidColor;
+    }
+    
+    void Update() {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        camera.backgroundColor = Color.Lerp(color1, color2, t);
+    }
+}

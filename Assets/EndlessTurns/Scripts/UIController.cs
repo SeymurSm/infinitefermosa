303f7ac049ab9095227bf69a4cee4c022221715e
SeymurSm <seymur.sm@gmail.com>
2021-06-29 11:21:00 +0300

    Rate dialog

--- Assets/EndlessTurns/Scripts/UIController.cs
+++ Assets/EndlessTurns/Scripts/UIController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
 
 
     public static UIController Instance;
@@ -14,32 +15,61 @@ public class UIController : MonoBehaviour {
     public Image replayButton;
     public PlayerController playerController;
     public GameObject rateMenu;
-    private int playCount ;
+    private int playCount;
     private bool alreadyDisplayedRate = false;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Instance = this;
         playCount = PlayerPrefs.GetInt("play_count");
-        
+
         ScoreManager.Instance.Reset();
         muteButton.enabled = false;
         unMuteButton.enabled = false;
         replayButton.enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         score.text = ScoreManager.Instance.Score.ToString();
         bestScore.text = ScoreManager.Instance.HighScore.ToString();
         gold.text = CoinManager.Instance.Coins.ToString();
 
         if (playerController.gameOver && !alreadyDisplayedRate)
         {
-	    alreadyDisplayedRate = true;
+            alreadyDisplayedRate = true;
             Invoke("RateDialog", 1.5f);
-	   
+
         }
-	}
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                OnBackPressed();
+            }
+        }
+    }
+
+
+    void OnBackPressed()
+    {
+        if (!alreadyDisplayedRate)
+        {
+            alreadyDisplayedRate = true;
+           //Invoke("RateDialog", 1.5f);
+            rateMenu.SetActive(true);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
 
 
     public void SoundClick()
@@ -61,42 +91,57 @@ public class UIController : MonoBehaviour {
 
     public void ReplayButton()
     {
-        PlayerPrefs.SetInt("play_count", playCount+1);
+        PlayerPrefs.SetInt("play_count", playCount + 1);
         SoundManager.Instance.PlaySound(SoundManager.Instance.hitButton);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        
+
     }
 
-    public void Rate(bool rate){
-        rateMenu.SetActive(false);
-        if(rate)
-            Application.OpenURL("http://unity3d.com/");
-        
-
-EnableButton();
-    }
-
-    public void RateDialog(){
-	if(playCount>0 && playCount%3 ==0 ){
-            rateMenu.SetActive(true);
-}
-       else
-	EnableButton();
-}
-
-    void EnableButton()
+    public void Rate(bool rate)
     {
-	rateMenu.SetActive(false);
-        replayButton.enabled = true;
-        if (SoundManager.Instance.IsMuted())
+        rateMenu.SetActive(false);
+        if (rate)
         {
-            muteButton.enabled = true;
-            unMuteButton.enabled = false;
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+                Application.OpenURL("http://unity3d.com/");
+            else if (Application.platform == RuntimePlatform.Android)
+                Application.OpenURL("https://play.google.com/store/apps/details?id=com.alvadigames.infinite_fermosa");
+
+
+            
         }
-        else
+
+        EnableButton();
+    }
+
+
+    public void NotNow(){
+         EnableButton();
+    }
+
+    public void RateDialog()
         {
-            muteButton.enabled = false;
-            unMuteButton.enabled = true;
+            if (playCount > 0 && playCount % 3 == 0)
+            {
+                rateMenu.SetActive(true);
+            }
+            else
+                EnableButton();
+        }
+
+        void EnableButton()
+        {
+            rateMenu.SetActive(false);
+            replayButton.enabled = true;
+            if (SoundManager.Instance.IsMuted())
+            {
+                muteButton.enabled = true;
+                unMuteButton.enabled = false;
+            }
+            else
+            {
+                muteButton.enabled = false;
+                unMuteButton.enabled = true;
+            }
         }
     }
-}
